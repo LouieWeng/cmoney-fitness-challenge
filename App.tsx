08a599d6b0b3c760653ed0eba198d394eac4e9fe
LouieWeng <louieweng58@gmail.com>
2025-07-25 10:47:53 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,31 @@
+
 import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import RankingPage from './pages/RankingPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App: React.FC = () => {
+  return (
+    <HashRouter>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/ranking" element={<RankingPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </HashRouter>
+  );
+};
+
+export default App;
+
+{/*import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 
@@ -29,4 +56,4 @@ const App: React.FC = () => {
   );
 };
 
-export default App;
+export default App;*/}
