7d68d8265de5c7e46c01e9cb99151d5f03f5560d
LouieWeng <louieweng58@gmail.com>
2025-07-25 10:18:02 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,27 @@
 
+{/*import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+*/}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { inject } from '@vercel/analytics';
+
+inject(); // 在這裡初始化 Analytics，不干擾畫面渲染
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
