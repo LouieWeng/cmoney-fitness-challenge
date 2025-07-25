d9c242f6a15141f46517fb385dcd07d324e1c318
LouieWeng <louieweng58@gmail.com>
2025-07-25 10:24:04 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import HomePage from './pages/HomePage';
 import RulesPage from './pages/RulesPage';
 import RankingPage from './pages/RankingPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-slate-900 text-white px-4 pt-20">
-        <Header />
-        <main className="flex-grow pt-12 md:pt-16 lg:pt-20 px-4">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/rules" element={<RulesPage />} />
-            <Route path="/ranking" element={<RankingPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="min-h-screen flex flex-col bg-slate-900 text-white">
+          <Header />
+          <main className="flex-grow pt-12 md:pt-16 lg:pt-20 px-4">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/rules" element={<RulesPage />} />
+              <Route path="/ranking" element={<RankingPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+      <Analytics /> {/* 放在 Router 外層就不會干擾版面 */}
+    </>
   );
 };
 
