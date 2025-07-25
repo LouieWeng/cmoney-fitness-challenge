5acc255f02ffcf5650cde3d572d78c5770fa8310
LouieWeng <louieweng58@gmail.com>
2025-07-25 10:58:01 +0800

    test

--- App.tsx
+++ App.tsx
@@ -1,29 +1,61 @@
-
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react'; /*???*/
+
 import HomePage from './pages/HomePage';
+import RulesPage from './pages/RulesPage'; /*???*/
 import RankingPage from './pages/RankingPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
-    <HashRouter>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/ranking" element={<RankingPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </HashRouter>
+    <>
+      <HashRouter>
+        <div className="flex flex-col min-h-screen">>
+          <Header />
+          <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              {/*<Route path="/rules" element={<RulesPage />} />*/}
+              <Route path="/ranking" element={<RankingPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </HashRouter>
+      <Analytics />
+    </>
   );
 };
 
 export default App;
+
+// import React from 'react';
+// import { HashRouter, Routes, Route } from 'react-router-dom';
+// import HomePage from './pages/HomePage';
+// import RankingPage from './pages/RankingPage';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+// const App: React.FC = () => {
+//   return (
+//     <HashRouter>
+//       <div className="flex flex-col min-h-screen">
+//         <Header />
+//         <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12">
+//           <Routes>
+//             <Route path="/" element={<HomePage />} />
+//             <Route path="/ranking" element={<RankingPage />} />
+//           </Routes>
+//         </main>
+//         <Footer />
+//       </div>
+//     </HashRouter>
+//   );
+// };
+
+// export default App;
 
 {/*import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
