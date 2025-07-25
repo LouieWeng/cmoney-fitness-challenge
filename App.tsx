2ae9d82e4cd74e7d7245d01531b52e30690597f7
LouieWeng <louieweng58@gmail.com>
2025-07-25 10:27:10 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RulesPage from './pages/RulesPage';
 import RankingPage from './pages/RankingPage';
