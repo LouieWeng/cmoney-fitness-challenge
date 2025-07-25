29b0e66588e425434a47896d34ed343f4c3c5edf
LouieWeng <louieweng58@gmail.com>
2025-07-25 11:34:07 +0800

    多打了一個>

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
   return (
     <>
       <HashRouter>
-        <div className="flex flex-col min-h-screen">>
+        <div className="flex flex-col min-h-screen">
           <Header />
           <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12">
             <Routes>
