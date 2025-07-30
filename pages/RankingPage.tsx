4251d9115d9733de2e1a8ae94f893edd4523dbc0
LouieWeng <louieweng58@gmail.com>
2025-07-30 17:56:10 +0800

    把編號改成淺灰色

--- pages/RankingPage.tsx
+++ pages/RankingPage.tsx
@@ -96,7 +96,9 @@ const RankingPage: React.FC = () => {
                       {getTrophyIcon(index + 1)}
                       <span className="hidden sm:inline">{index + 1}</span>
                     </td>
-                    <td className="px-6 py-4 whitespace-nowrap font-medium text-white">#{team.id} {team.name}</td>
+                    <td className="px-6 py-4 whitespace-nowrap font-medium text-white">
+                      <span className="text-slate-400">#{team.id}</span> {team.name}
+                    </td>
                     <td className="px-6 py-4 whitespace-nowrap text-white">
                       {team.members.join(' & ')}
                     </td>
