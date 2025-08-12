312fae39e670a011a5cab8726d5a4d41a3971106
LouieWeng <louieweng58@gmail.com>
2025-08-12 13:46:38 +0800

    更新成績

--- pages/RankingPage.tsx
+++ pages/RankingPage.tsx
@@ -42,7 +42,7 @@ const RankingPage: React.FC = () => {
           每週五前將結算前一週的運動打卡分數，並且更新到賽況。
         </p>
         <p className="mt-3 max-w-2xl mx-auto text-lg text-slate-300">
-          最近更新：2025/08/04
+          最近更新：2025/08/12
         </p>
       </section>
 
